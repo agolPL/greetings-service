32f7d10eb2b2f5d77bcb84768fb1345022fad187
agolPL <andipansa@gmail.com>
2019-12-18 09:30:20 +0100

    init project

---
+++ src/main/java/pl/osec/openshift/example/greetings/GreetingsServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.osec.openshift.example.greetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/osec/openshift/example/greetings/GreetingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.osec.openshift.example.greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
